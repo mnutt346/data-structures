91db46b2dc9721c3d010cc797c79ed0c3e4b5ba0
mnutt346 <mnutt346@gmail.com>
2018-11-07 17:00:18 -0600

    function queue passing tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,49 @@
+// FIRST IN FIRST OUT !*!
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var low; 
+  var high;
+
+
   // Implement the methods below
 
+
+  // PUSH
   someInstance.enqueue = function(value) {
+    if(Object.keys(storage).length === 0) {
+      low = 0;
+      high = 1;
+      storage[0] = value;
+    } else {
+      storage[high] = value;
+      high++
+    }
   };
 
+  // POP
   someInstance.dequeue = function() {
+    if(Object.keys(storage).length === 0) {
+      return 0;
+    } else {
+      var dq = storage[low];
+      delete storage[low];
+      low++
+    }
+    return dq;
   };
 
+  // SIZE
   someInstance.size = function() {
+    if(Object.keys(storage).length === 0) {
+      return 0;
+    } else {
+      return high - low;
+    }
   };
 
   return someInstance;
