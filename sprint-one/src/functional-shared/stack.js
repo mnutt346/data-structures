2a3f08f0a096237341ae6d4bcffa06e0dc4fb6c3
mnutt346 <mnutt346@gmail.com>
2018-11-07 16:38:28 -0600

    Most tests pasing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var lastKey;
+  Stack.push = stackMethods.push;
+  Stack.pop = stackMethods.pop;
+  Stack.size = stackMethods.size;
+  return stackMethods;
 };
 
 var stackMethods = {};
+var storage = {};  
 
+stackMethods.push = function (value) {
+  console.log('*****PUSH*****');
+  if (Object.keys(storage).length === 0) {
+    storage[0] = value;
+    lastKey = 0;
+  } else {
+    lastKey++;
+    storage[lastKey] = value;
+  }
+}
 
+stackMethods.pop = function () {
+  console.log('*****POP*****');
+  if (Object.keys(storage).length === 0) {
+    return 0;
+  } else {
+    var popVal = storage[lastKey];
+    delete storage[lastKey];
+    lastKey--;
+  }
+  return popVal;
+}
+
+stackMethods.size = function () {
+  if (Object.keys(storage).length === 0) {
+    return 0;
+  } else {
+    return Object.keys(storage).length;
+  }
+}
