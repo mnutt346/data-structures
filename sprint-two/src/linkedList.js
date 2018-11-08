08038eedc792cbbe52a5ec329c8a346000519cb6
mnutt346 <mnutt346@gmail.com>
2018-11-08 15:59:10 -0600

    linked lists passing with flying colors

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    
+    } else if (list.head === list.tail) {
+      list.tail = newNode;
+      list.head.next = list.tail;
+    } else {
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    var newHead = list.head.next
+    delete list.head;
+    list.head = newHead;
+    return headValue
   };
 
   list.contains = function(target) {
+    var answer = false;
+    var recursion = function (node) {
+      if(node.value === target) {
+        answer = true;
+        return;
+      } else {
+        if(node.next) {
+          recursion(node.next)
+        }
+      }
+    }
+
+    if (list.head === null) {
+      return false;
+    } else if (list.head.value === target) {
+      return true
+    } else {
+      if(list.head.next) {
+        recursion(list.head.next);
+      }
+    }
+    return answer
   };
 
   return list;
