381f94e418d270eda32bfc6658c2b20e7267858d
mnutt346 <mnutt346@gmail.com>
2018-11-09 10:36:37 -0600

    SET is passing

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,38 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(this._storage[item]) {
+  } else {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  for(var name in this._storage) {
+    if(this._storage[name] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for(var name in this._storage) {
+    if(this._storage[name] === item) {
+      delete this._storage[name]
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// ADD: constant
+// CONTAINS: linear
+// REMOVE: linear
